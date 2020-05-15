9d24e5bfd72da2ef964c39a069fc26ed735b3e68
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-05-15 20:21:53 +0900

    commit

--- level1/NonCompletor.java
+++ level1/NonCompletor.java
@@ -3,21 +3,36 @@ import java.util.*;
 
 public class NonCompletor {
     public static void main(String[] args){
-        String[] participant = {"leo", "kiki", "eden"};
-        String[] completion ={"eden", "kiki"};
+        String[] participant = {"kiki ", "kiki", "asd"};
+        String[] completion ={"kiki", "kiki"};
         System.out.println(solution(participant,completion));
     }
 
     public static String solution(String[] participant, String[] completion){
         String answer= "";
-        HashSet<String> set = new HashSet<>();
+        String[] tempP = participant;
+        String[] tempC = completion;
+        Arrays.sort(tempP);
+        Arrays.sort(tempC);
 
-        for(int i=0; i<completion.length; i++){
-            set.add(completion[i]);
+        for(int i=0; i<tempC.length; i++){
+            if(tempC[i].equals(tempP[i])){
+                if(i+1==tempC.length){
+                    answer= tempP[i+1];
+                }
+            }
+            else{
+                answer= tempP[i];
+                break;
+            }
         }
 
-        
-
         return answer;
+    }
+
+    public static void print(String[] array){
+        for(int i=0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
     }
 }
