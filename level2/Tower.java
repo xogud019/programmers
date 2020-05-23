3e185eef828eddc87889f4ce80ccfdfcba52f670
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-05-23 15:38:49 +0900

    commit

--- level2/Tower.java
+++ level2/Tower.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Tower {
     public static void main(String[] args){
-        int[] heights ={6,9,5,7,7};
+        int[] heights ={1,5,3,6,7,6,5};
         int[] result = solution(heights);
         for(int i=0; i<result.length; i++){
             System.out.println(result[i]);
@@ -11,18 +11,14 @@ public class Tower {
     }
 
     public static int[] solution(int[] heights){
-        Stack<Integer> stack = new Stack<>();
-        int[] answer= new int[heights.length];
-        int index = heights.length-1;
-
+        int[] answer = new int[heights.length];
         for(int i=heights.length-1; i>0; i--){
-            if(stack.isEmpty()){
-                stack.add(heights[i]);
-            }
-            else{
-                if(stack.lastElement()<heights[i]){
-                    
+            for(int j=i; j>0; j--){
+                if(heights[i]<heights[j-1]){
+                    answer[i]=j;
+                    break;
                 }
+
             }
         }
         return answer;
