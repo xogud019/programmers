072b2d8674f72efeb41c229fc684d1ecb9b80621
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-05-24 22:38:36 +0900

    commit

--- level2/Printer.java
+++ level2/Printer.java
@@ -3,13 +3,44 @@ import java.util.*;
 
 public class Printer {
     public static void main(String[] args){
-        int[] priorities={1,1,9,1,1,1};
-        int location = 0;
+        int[] priorities={2,1,3,2};
+        int location = 2;
         //char c = 'A';
         //System.out.println((char)((int)c+1));
         System.out.println(solution(priorities, location));
     }   
     
+    public static int solution(int[] priorities, int location){
+        int answer= 1;
+        int n = priorities.length;
+        int[] arr = new int[n];
+        
+        for(int i=0; i<n; i++){
+            arr[i]=priorities[i];
+        }
+        Arrays.sort(arr);
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=n-1; i>=0; i--){
+            q.add(arr[i]);
+        }
+        
+        while(!q.isEmpty()){
+            for(int i=0; i<n; i++){
+                if(priorities[i]==q.peek()){
+                    if(i==location){
+                        return answer;
+                    }
+                    q.poll();
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    
+    }
+    
+       /*
     public static int solution(int[] priorities, int location){
         int answer= 1;
         int n = priorities.length;
@@ -32,4 +63,5 @@ public class Printer {
         }
         return answer;
     }
+    */
 }
