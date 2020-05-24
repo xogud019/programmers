2543b48870a81da9040c68964d03d6bda985b64f
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-05-24 21:36:35 +0900

    commit

--- level2/Printer.java
+++ level2/Printer.java
@@ -3,43 +3,33 @@ import java.util.*;
 
 public class Printer {
     public static void main(String[] args){
-        int[] priorities={2,1,3,2};
-        int location = 2;
+        int[] priorities={1,1,9,1,1,1};
+        int location = 0;
         //char c = 'A';
         //System.out.println((char)((int)c+1));
         System.out.println(solution(priorities, location));
     }   
     
     public static int solution(int[] priorities, int location){
-        char[] paper = new char[priorities.length];
-        Queue<Integer> prio = new LinkedList<>();
-        Queue<Character> pap = new LinkedList<>();
-        int answer= 0;
-        int[] arr= new int[paper.length];
-        for(int i=0; i<paper.length; i++){
-            paper[i]=(char)((int)'A'+i);
-            pap.add(paper[i]);
-            prio.add(priorities[i]);
+        int answer= 1;
+        int n = priorities.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i=0; i<n; i++){
+            pq.add(priorities[i]);
         }
 
-        for(int i=0; i<paper.length; i++){
-            int temp = prio.poll();
-            int count=0;
-            for(int j=0; j<paper.length-1; j++){
-                if(temp<priorities[j]){
-                    prio.add(temp);
-                    count++;
+        while(!pq.isEmpty()){
+            for(int i=0; i<n; i++){
+                if(priorities[i]==pq.peek()){
+                    if(i==location){
+                        return answer;
+                    }
+                    pq.poll();
+                    answer++;
                 }
             }
-            if(count==0){
-                arr[i]=temp;
-            }
         }
-
-        for(int i=0; i< arr.length; i++){
-            System.out.println(arr[i]);
-        }
-
         return answer;
     }
 }
