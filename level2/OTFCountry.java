9f2befb3bc66f3e3f434d3ae4609d046c7387e3a
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-05-27 18:23:05 +0900

    commit

---
+++ level2/OTFCountry.java
@@ -0,0 +1,26 @@
+package level2;
+
+public class OTFCountry {
+    public static void main(String[] args){
+        int n=15;
+        System.out.println(solution(n));
+    }
+
+    public static String solution(int n){
+        String answer ="";
+        int rest = 0;
+        int share = n;
+        while(share!=0){
+            rest = share%3;
+            share = share/3;
+            if(rest==0){
+                answer="4"+answer;
+                share--;
+            }
+            else{
+                answer= Integer.toString(rest)+answer;
+            }
+        }
+        return answer;
+    }
+}
