097efe0030587bc3885f8ed1a6ee47dc62f171f6
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-05-31 20:14:35 +0900

    commit

---
+++ level2/Fibo.java
@@ -0,0 +1,31 @@
+package level2;
+
+public class Fibo {
+    public static void main(String[] args){
+        int n  =100;
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n){
+        long x1 = 0;
+        long x2 = 1;
+        
+        return (int)fibo(x1,x2,n,0);
+    }
+
+    public static long fibo(long x1, long x2, int count, int index){
+        long f1 = x1;
+        long f2 = x2;
+
+        if(index==count){
+            return f1;
+        }
+        else{
+            long f3 = (f1+f2)%1234567;
+            f1 = f2;
+            f2 = f3;
+            index++;
+            return fibo(f1, f2, count, index);
+        }
+    }
+}
---
+++ level2/MoreSpicy.java
@@ -0,0 +1,31 @@
+package level2;
+import java.util.*;
+
+public class MoreSpicy {
+    public static void main(String[ ]args){
+        int[] scoville ={1,1,1};
+        int K = 9;
+        System.out.println(solution(scoville, K));
+    }
+
+    public static int solution(int[] scoville, int K){
+        int answer = 0;
+        
+        Queue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<scoville.length; i++){
+            pq.add(scoville[i]);
+        }
+        while(true){
+            if(pq.size()==1){
+                return -1;
+            }
+            pq.add(pq.poll()+pq.poll()*2);
+            answer++;
+            if(pq.peek()>=K){
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
