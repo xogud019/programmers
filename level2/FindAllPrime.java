f3103032835e13dd2a4887172f2779c93ed8b98c
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-05-31 21:02:29 +0900

    commit

---
+++ level2/FindAllPrime.java
@@ -0,0 +1,16 @@
+package level2;
+
+public class FindAllPrime {
+    public static void main(String[] args){
+        String numbers = "011";
+        System.out.println(solution(numbers));
+    }
+
+    public static int solution(String numbers){
+        int answer= 0;
+        String[] arr = numbers.split("");
+        
+        System.out.println(arr.length<<1);
+        return answer;
+    }
+}
