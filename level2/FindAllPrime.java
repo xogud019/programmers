2ddc7c7e25581ea43deb5d189d5a1bb62d02127d
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-02 20:18:42 +0900

    commit

--- level2/FindAllPrime.java
+++ level2/FindAllPrime.java
@@ -8,23 +8,59 @@ public class FindAllPrime {
     }
 
     public static int solution(String numbers){
-        int answer= 0;
         String[] arr = numbers.split("");
         List<Integer> list = new ArrayList<>();
+        String s = "";
+        int maxNum = 0;
+        Arrays.sort(arr,Collections.reverseOrder());
         for(int i=0; i<arr.length; i++){
-            list.add(Integer.parseInt(arr[i]));
+            s+=arr[i];
         }
 
-        Arrays.sort(list,Collections.reverseOrder());
-        boolean[] check = new boolean[arr.length];
-        for(int i=0; i<check.length; i++){
-            check[i]=false;
+        maxNum = Integer.parseInt(s);
+        int[] primeArr = new int[maxNum+1];
+
+        for(int i=2; i<=maxNum; i++){
+            for(int j=i*2; j<=maxNum; j+=i){
+                primeArr[j]=1;
+            }
         }
+        //System.out.println(maxNum);
+        //System.out.println(isPrime(3));
+        for(int i=2; i<=maxNum; i++){
+            boolean isP = true;
+            if(primeArr[i]==0){
+                String[] iArr = Integer.toString(i).split("");
+                for(int j=0; j<iArr.length; j++){
+                    if(!numbers.contains(iArr[j])){
+                        isP = false;
+                        break;
+                    }
+                }
 
-        return answer;
-    }
+                StringBuilder temp = new StringBuilder(numbers);
+                for(int k =0; k<iArr.length; k++){
+                    int idx = temp.indexOf(iArr[k]);
+                    if(idx == -1){
+                        isP = false;
+                        break;
+                    }
+                    else{
+                        temp.setCharAt(idx, '*');
+                    }
+                }
 
-    public static void findPrime(String[] arr, boolean[] check){
-
+                if(isP){
+                    list.add(i);
+                }
+            }
+            else continue;
+        }
+        /*
+        for(int i=0; i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        */
+        return list.size();
     }
 }
