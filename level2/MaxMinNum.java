0270c94a33bddbef627f20737a152fc21e6cae91
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-04 21:40:48 +0900

    commit

---
+++ level2/MaxMinNum.java
@@ -0,0 +1,25 @@
+package level2;
+import java.util.*;
+
+public class MaxMinNum {
+    public static void main(String[] args){
+        String s = "1 2 3 4";
+        System.out.println(solution(s));
+    }
+
+    public static String solution(String s) {
+        String answer = "";
+        String[] arr = new String[s.length()];
+        Queue<Integer> orderQ = new PriorityQueue<>();
+        Queue<Integer> rOrderQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        arr = s.split(" ");
+
+        for(int i=0; i<arr.length; i++){
+            orderQ.add(Integer.parseInt(arr[i]));
+            rOrderQ.add(Integer.parseInt(arr[i]));
+        }
+        answer = Integer.toString(orderQ.poll())+" "+rOrderQ.poll();
+        return answer;
+    }
+}
