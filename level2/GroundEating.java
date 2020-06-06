1e38e822c56e42e8d5f3e04c59aa5de7cf70c396
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-06 20:21:22 +0900

    commit

---
+++ level2/GroundEating.java
@@ -0,0 +1,13 @@
+package level2;
+
+public class GroundEating {
+    public static void main(String[] args){
+
+    }
+
+    public static int solution(int[][] land){
+        int answer = 0;
+
+        return answer;
+    }
+}
