d7ef4d9a9f327f65c706824fd226047aa7914d67
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-07 19:35:25 +0900

    commit

--- level2/GroundEating.java
+++ level2/GroundEating.java
@@ -1,13 +1,24 @@
 package level2;
+import java.util.*;
 
 public class GroundEating {
     public static void main(String[] args){
-
+        int[][] land = {{1,2,3,5},{5,6,7,8,},{4,3,2,1}};
+        System.out.println(solution(land));
     }
 
     public static int solution(int[][] land){
-        int answer = 0;
+        for(int i=1; i<land.length; i++){
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+        }
 
-        return answer;
+        Arrays.sort(land[land.length-1]);
+        
+        return land[land.length-1][3];
     }
+
+    
 }
