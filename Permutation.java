11fa0740a0229a035c486e89fc3e7b334c1707e8
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-13 20:03:59 +0900

    commit

---
+++ Permutation.java
@@ -0,0 +1,33 @@
+public class Permutation {
+    public static void main(String[] args){
+        char[] s = {'A','B','C'};
+        perm(s, 0, s.length);
+        String ss = "s";
+        
+    }   
+
+    public static void perm(char[] s, int depth, int max){
+        if(depth==max){
+            print(s);
+            return;
+        }
+        for(int i=depth; i<s.length; i++){
+            swap(s, i, depth);
+            perm(s, depth+1, max);
+            swap(s, i, depth);
+        }
+    }
+
+    public static void print(char[] s){
+        for(int i=0; i<s.length; i++){
+            System.out.print(s[i]);
+        }
+        System.out.println();
+    }
+
+    public static void swap(char[] s, int i, int j){
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
--- level2/ColorBook.java
+++ level2/ColorBook.java
@@ -20,51 +20,8 @@ public class ColorBook {
         Stack<Integer> XStack = new Stack<>();
         Stack<Integer> YStack = new Stack<>();
         
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                int count = 0;
-                if(picture[i][j]!=0&&!visited[i][j]){
-                    XStack.push(j);
-                    YStack.push(i);
-                    visited[i][j]=true;
-                    count++;
-                    answer[0]++;
-                }
-
-                while(!XStack.isEmpty()){
-                    int x = XStack.pop();
-                    int y = YStack.pop();
-
-                    if(y>0&&picture[y-1][x]==picture[i][j]&&!visited[y-1][x]){
-                        XStack.push(x);
-                        YStack.push(y-1);
-                        count++;
-                        visited[y-1][x] = true;
-                    }
-                    if(x>0&&picture[y][x-1]==picture[i][j]&&!visited[y][x-1]){
-                        XStack.push(x-1);
-                        YStack.push(y);
-                        count++;
-                        visited[y][x-1] = true;
-                    }
-                    if(y<m-1&&picture[y+1][x]==picture[i][j]&&!visited[y+1][x]){
-                        XStack.push(x);
-                        YStack.push(y+1);
-                        count++;
-                        visited[y+1][x] = true;
-                    }
-                    if(x<n-1&&picture[y][x+1]==picture[i][j]&&!visited[y][x+1]){
-                        XStack.push(x+1);
-                        YStack.push(y);
-                        count++;
-                        visited[y][x+1] = true;
-                    }
-                }
-
-                answer[1] = Math.max(answer[1],count);
-            }
-        }
+        
+        
         return answer;
     }
 }
