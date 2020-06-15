1781dd7d02bc07aa3dd052d48ba8d7a8e7f05cd8
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-15 16:58:08 +0900

    commit

--- level2/ColorBook.java
+++ level2/ColorBook.java
@@ -22,8 +22,51 @@ public class ColorBook {
         
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
+                int count = 0;
                 if(picture[i][j]!=0&&!visited[i][j]){
+                    XStack.push(j);
+                    YStack.push(i);
+                    visited[i][j]= true;
+                    count++;
+                    answer[0]++;
+                }
+
+                while(!XStack.isEmpty()){
+                    int x = XStack.pop();
+                    int y = YStack.pop();
+
+                    if(y>0&&picture[y-1][x]==picture[i][j]&&!visited[y-1][x]){
+                        XStack.push(x);
+                        YStack.push(y-1);
+                        visited[y-1][x]=true;
+                        count++;
+                    }
                     
+                    if(y<m-1&&picture[y+1][x]==picture[i][j]&&!visited[y+1][x]){
+                        XStack.push(x);
+                        YStack.push(y+1);
+                        visited[y+1][x]=true;
+                        count++;
+
+                    }
+                    
+                    if(x>0&&picture[y][x-1]==picture[i][j]&&!visited[y][x-1]){
+                        XStack.push(x-1);
+                        YStack.push(y);
+                        visited[y][x-1]=true;
+                        count++;
+
+                    }
+                    if(x<n-1&&picture[y][x+1]==picture[i][j]&&!visited[y][x+1]){
+                        XStack.push(x+1);
+                        YStack.push(y);
+                        visited[y][x+1]=true;
+                        count++;
+
+                    }
+                }
+                if(answer[1]<count){
+                    answer[1]=count;
                 }
             }
         }
