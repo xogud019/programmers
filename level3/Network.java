5f525b9d9e851ffa923db1d1929fa808a4d5b5c3
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-15 17:00:03 +0900

    commit

--- level3/Network.java
+++ level3/Network.java
@@ -8,25 +8,7 @@ public class Network {
     }   
 
     public static int solution(int n, int[][] computers){
-        int answer = 0;
-        boolean[][] visited = new boolean[n][n];
-
-        for(int i=0; i<n; i++){
-            if(!visited[i][i]){
-                dfs(i, n ,computers, visited);
-                answer++;
-            }
-        }
-        return answer;
-    }
-
-    public static void dfs(int index, int n, int[][] computers,boolean[][] visited){
-        for(int i=0; i<n; i++){
-            if(computers[index][i]==1&&!visited[index][i]){
-                visited[index][i] = visited[i][index] = true;
-                dfs(i,n,computers, visited);
-            }
-        }
+        
     }
 
 }
