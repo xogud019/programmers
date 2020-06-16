806c36e72bf24a874317923658709dcb3f7316f0
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-17 02:49:15 +0900

    commit

--- level2/FileNameSort.java
+++ level2/FileNameSort.java
@@ -1,4 +1,5 @@
 package level2;
+import java.util.*;
 
 public class FileNameSort {
     public static void main(String[] args){
@@ -11,16 +12,57 @@ public class FileNameSort {
     }
 
     public static String[] solution(String[] files) {
-        String[] answer = {};
-        String head ="";
-        String num ="";
-        String tail ="";
+        Arrays.sort(files, new Comparator<String>(){
+            @Override
+            public int compare(String s1, String s2){
+                String[] file1 = disunite(s1);
+                String[] file2 = disunite(s2);
 
-        for(int i=0; i<files[0].length(); i++){
-            if(Character.isDigit(files[0].charAt(i))){
-                num +=""+files[0].charAt(i);
+                int headVal = file1[0].compareTo(file2[0]);
+
+                if(headVal==0){
+                    int num1 = Integer.parseInt(file1[1]);
+                    int num2 = Integer.parseInt(file2[1]);
+
+                    return num1-num2;
+                }
+                else{
+                    return headVal;
+                }
             }
-        }
-        return answer;
+
+            public String[] disunite(String s){
+                String head ="";
+                String number ="";
+                String tail ="";
+                int index = 0;
+
+                for(; index<s.length(); index++){
+                    char c = s.charAt(index);
+                    if(c<'0'||c>'9'){
+                        head+=c;
+                    }        
+                    else break;
+                }
+
+                for(; index<s.length(); index++){
+                    char c = s.charAt(index);
+                    if(c>='0'&&c<='9'){
+                        number+=c;
+                    }        
+                    else break;        
+                }
+
+                for(; index<s.length(); index++){
+                    char c = s.charAt(index);    
+                    tail+=c;    
+                }
+
+                String[] file = {head.toUpperCase(),number , tail};
+                return file;
+            }
+        });
+
+        return files;
     }
 }
