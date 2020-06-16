91312eba2fed53b7f32c598df026462c64413d3f
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-16 21:23:21 +0900

    commit

--- level2/Compress.java
+++ level2/Compress.java
@@ -12,21 +12,39 @@ public class Compress {
 
     public static int[] solution(String msg) {
         int[] answer = {};
-        int index = 27;
-        Map <Character, Integer> dic = new HashMap<>();
+        int index = 1;
+        int dicIdx =27;
+        Map <String, Integer> dic = new HashMap<>();
         List<Integer> temp = new ArrayList<>();
+        List<Integer> idx = new ArrayList<>();
 
-        //Make Dictionary
         for(int i=0; i<26; i++){
-            dic.put((char)('A'+i), i+1);
+            dic.put(Character.toString((char)('A'+i)), i+1);
+        }
+        /*
+        for(String c: dic.keySet()){
+            System.out.println(c+","+dic.get(c));
+        }
+        */
+        String start = Character.toString(msg.charAt(0));
+        while(index<msg.length()){
+            String next = Character.toString(msg.charAt(index));
+            if(dic.containsKey(start+next)){
+                start+=next;
+                index++;
+            }
+            else{
+                idx.add(dic.get(start));
+            }
+            System.out.println(start+","+next+","+start+next);
+            dic.put(start+next, dicIdx);
+            start = next;
+            dicIdx++;
+            index++;
         }
 
-        for(int i=0; i<msg.length(); i++){
-            char s = msg.charAt(i);
-            if(dic.containsKey(s)){
-                temp.add(dic.get(s));
-                
-            }
+        for(int i=0; i<idx.size(); i++){
+            System.out.println(idx.get(i));
         }
         return answer;
     }
