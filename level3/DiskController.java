6ec03a99bfeb11da415c6ce1b25efaab358fd414
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-20 01:26:04 +0900

    commit

--- level3/DiskController.java
+++ level3/DiskController.java
@@ -2,22 +2,6 @@ package level3;
 import java.util.*;
 
 public class DiskController {
-    public static void main(String[] args){
-        int[][] jobs = {{0,3},{1,9},{2,6}};
-
-        System.out.println(solution(jobs));
-    }
-
-    public static int solution(int[][] jobs){
-        int answer = 0;
-        Queue<Job> pq = new PriorityQueue<>();
-
-
-        return answer;
-    }
-
-    class Job{
-        int req=0;
-        int pro=0;
-    }
+    
+    
 }
