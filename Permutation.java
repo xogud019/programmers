55878e087052438e5071569357b14a5eeddadbe3
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-22 21:02:08 +0900

    commit

--- Permutation.java
+++ Permutation.java
@@ -1,6 +1,6 @@
 public class Permutation {
     public static void main(String[] args){
-        char[] s = {'A','B','C'};
+        char[] s = {'A','B','C','D'};
         perm(s, 0, s.length);        
     }   
 
