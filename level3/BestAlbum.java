96ed55e991034c42f5d09626e7fb0f0e231abd99
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-30 21:26:03 +0900

    commit

--- level3/BestAlbum.java
+++ level3/BestAlbum.java
@@ -3,50 +3,109 @@ import java.util.*;
 //not
 public class BestAlbum {
     public static void main(String[] args){
-        String[] genres = {"classic","pop","classic","classic","pop"};
-        int[] plays={500,600,150,800,2500};
+        String[] genres = {"classic","pop","classic","pop","classic"};
+        int[] plays={500,600,800,2500,500};
         int[] result = solution(genres, plays);
 
         for(int i=0; i<result.length; i++){
-            System.out.println(result[i]);
+            System.out.print(result[i]+" ");
         }
     }
 
     public static int[] solution(String[] genres, int[] plays){
-        int[] answer ={};
-        Comparator<String> comparator = (val1,val2)->val2.compareTo(val1);
-        Map<String, String> tolPlay = new TreeMap<>(comparator);
+        Map<String, Integer> tolPlay = new HashMap<>();
         Map<Integer, String> gMap = new HashMap<>();
         Map<Integer, Integer> pMap = new HashMap<>();
-        Set<String> gen = new HashSet<>();
+        Comparator<Integer> comparator = (s1, s2)->s2.compareTo(s1);
+        Map<Integer, String> sortMap = new TreeMap<>(comparator);
 
         for(int i=0; i<plays.length; i++){
             if(tolPlay.containsKey(genres[i])){
-                tolPlay.put(genres[i], Integer.toString(Integer.parseInt(tolPlay.get(genres[i]))+plays[i]));
+                tolPlay.put(genres[i], tolPlay.get(genres[i])+plays[i]);
             }
             else{
-                tolPlay.put(genres[i], Integer.toString(plays[i]));
+                tolPlay.put(genres[i],plays[i] );
             }
             gMap.put(i+1, genres[i]);
             pMap.put(i+1, plays[i]);
-            gen.add(genres[i]);
+        }
+
+        for(String k:tolPlay.keySet()){
+            sortMap.put(tolPlay.get(k), k);
+        }
+
+        List<String> genList = new ArrayList<>(sortMap.values());
+        List<Integer> temp;
+        List<Integer> index = new ArrayList<>();
+
+        for(String k:genList){
+            temp = new ArrayList<>();
+            for(int i:gMap.keySet()){
+                if(k.equals(gMap.get(i))){
+                    temp.add(pMap.get(i));
+                }
+            }
+            
+            if(temp.size()!=1){
+                Collections.sort(temp, Collections.reverseOrder());
+                
+                for(int i=0; i<2; i++){
+                    for(int j:pMap.keySet()){
+                        if(temp.get(i)==pMap.get(j)){
+                            index.add(j-1);
+                            pMap.put(j, 0);
+                            
+                        }
+                    }
+                }
+            }
+            else{
+                for(int j:pMap.keySet()){
+                    if(temp.get(0)==pMap.get(j)){
+                        index.add(j-1);
+                        pMap.put(j, 0);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        int[] answer = new int[index.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i]=index.get(i);
         }
         
-        List<String> genList = new ArrayList<>(tolPlay.keySet());
-
+        /*
         for(String k:genList){
             System.out.println(k);
         }
+        */
+        /*
+        for(int k:gIndex){
+            System.out.println(k);
+        }
+        for(int k:pIndex){
+            System.out.println(k);
+        }
+        */
+        /*
+        for(int i:sortMap.keySet()){
+            System.out.println(i+","+sortMap.get(i));
+        }
+        */
         /*
         for(String k:tolPlay.keySet()){
             System.out.println(k+","+tolPlay.get(k));
         }
-        for(String k:gen){
-            System.out.println(k);
-        }
+        */
+        /*
         for(Integer k:gMap.keySet()){
             System.out.println(k+","+gMap.get(k));
         }
+        */
+        /*
+        System.out.println();
         for(Integer k:pMap.keySet()){
             System.out.println(k+","+pMap.get(k));
         }
