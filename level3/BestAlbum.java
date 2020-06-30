e894adad034effe4f5a2864a0645f19a17e92071
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-06-30 14:46:24 +0900

    commit

--- level3/BestAlbum.java
+++ level3/BestAlbum.java
@@ -14,25 +14,35 @@ public class BestAlbum {
 
     public static int[] solution(String[] genres, int[] plays){
         int[] answer ={};
-        Map<String, Integer> tolPlay = new HashMap<>();
+        Comparator<String> comparator = (val1,val2)->val2.compareTo(val1);
+        Map<String, String> tolPlay = new TreeMap<>(comparator);
         Map<Integer, String> gMap = new HashMap<>();
         Map<Integer, Integer> pMap = new HashMap<>();
+        Set<String> gen = new HashSet<>();
+
         for(int i=0; i<plays.length; i++){
             if(tolPlay.containsKey(genres[i])){
-                tolPlay.put(genres[i], tolPlay.get(genres[i])+plays[i]);
+                tolPlay.put(genres[i], Integer.toString(Integer.parseInt(tolPlay.get(genres[i]))+plays[i]));
             }
             else{
-                tolPlay.put(genres[i], plays[i]);
+                tolPlay.put(genres[i], Integer.toString(plays[i]));
             }
-        }
-
-        for(int i=0; i<plays.length; i++){
             gMap.put(i+1, genres[i]);
             pMap.put(i+1, plays[i]);
+            gen.add(genres[i]);
         }
+        
+        List<String> genList = new ArrayList<>(tolPlay.keySet());
 
+        for(String k:genList){
+            System.out.println(k);
+        }
+        /*
         for(String k:tolPlay.keySet()){
             System.out.println(k+","+tolPlay.get(k));
+        }
+        for(String k:gen){
+            System.out.println(k);
         }
         for(Integer k:gMap.keySet()){
             System.out.println(k+","+gMap.get(k));
@@ -40,6 +50,7 @@ public class BestAlbum {
         for(Integer k:pMap.keySet()){
             System.out.println(k+","+pMap.get(k));
         }
+        */
         return answer;
     }
 
