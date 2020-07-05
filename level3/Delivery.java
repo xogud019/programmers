b971409a5127e6b5d569216720de9c4787c797f0
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-07-05 22:14:34 +0900

    commit

--- level3/Delivery.java
+++ level3/Delivery.java
@@ -1,8 +1,10 @@
 package level3;
 import java.util.*;
-
+//study dijkstra
+//not
 public class Delivery {
-    //not
+    static List<Integer> vertax = new ArrayList<>();
+    
     public static void main(String[] args){
         int N = 5;
         int K = 3;
@@ -12,71 +14,80 @@ public class Delivery {
     }
 
     public static int solution(int N, int[][] road, int K) {
-        int[] parent = new int[N+1];
-        Set<Integer> edge = new TreeSet<>();
+        int answer = 0;
+        Graph g = new Graph(N);
 
-        edge.add(1);
-
-        Arrays.sort(road,new Comparator<int[]>(){
-            @Override
-            public int compare(int[] a1, int[] a2){
-                int val1 = a1[0];
-                int val2 = a2[0];
-                
-                return val1-val2;
-            }
-        });
-        
-        print(road);
         for(int i=0; i<road.length; i++){
-            if(edge.contains(road[i][0])){
-                
-            }
-            else if(edge.contains(road[i][1])){
+            g.input(road[i][0], road[i][1],road[i][2]);
+        }
+    
+        g.dijkstra(1);
 
+        for(int i:vertax){
+            if(i<=K){
+                answer++;
             }
         }
-        /*
-        for(int i=0; i<road.length; i++){
-            if(findSet(parent, road[i][0])==findSet(parent, road[i][1])){
-                continue;
+        return answer;
+    }
+
+    static class Graph{
+        int n;
+        int[][] map;
+
+        public Graph(int n){
+            this.n = n;
+            this.map = new int[n+1][n+1];
+        }
+
+        public void input(int i, int j, int w){
+            map[i][j] = w;
+            map[j][i] = w;
+        }
+
+        public void dijkstra(int v){
+            int[] distance = new int[n+1];
+            boolean[] visited = new boolean[n+1];
+
+            for(int i=1; i<=n; i++){
+                distance[i]=Integer.MAX_VALUE;
             }
-            else{
-                if(road[i][2]<=3){
-                    union(parent, road[i][0], road[i][1]);
-                    edge.add(road[i][0]);
-                    edge.add(road[i][1]);
+
+            distance[v] = 0;
+            visited[v] = true;
+
+            for(int i=1; i<=n; i++){
+                if(!visited[i]&&map[v][i]!=0){
+                    distance[i] = map[v][i];
                 }
             }
-        }
-        */
-        return edge.size();
-    }
 
-    public static int findSet(int[] parent, int node){
-        if(parent[node]==node){
-            return node;
-        }
-        else{
-            return parent[node]=findSet(parent, parent[node]);
-        }
-    }
+            for(int i=0; i<n-1; i++){
+                int min = Integer.MAX_VALUE;
+                int idx = -1;
 
-    public static void union(int[] parent, int node1, int node2){
-        int a = findSet(parent, node1);
-        int b = findSet(parent, node2);
+                for(int j=1; j<=n; j++){
+                    if(!visited[j]&&distance[j]!=Integer.MAX_VALUE){
+                        if(distance[j]<min){
+                            min = distance[j];
+                            idx = j;
+                        }
+                    }
+                }
 
-        if(a!=b){
-            parent[b] = a;
-        }
-    }
-
-    public static void print(int[][] arr){
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[i].length; j++){
-                System.out.print(arr[i][j]+" ");
+                visited[idx] = true;
+                for(int j=1; j<=n; j++){
+                    if(!visited[j]&&map[idx][j]!=0){
+                        if(distance[j]>distance[idx]+map[idx][j]){
+                            distance[j]=distance[idx]+map[idx][j];
+                        }
+                    }
+                }
             }
-            System.out.println();
+
+            for(int i =1; i<=n; i++){
+                vertax.add(distance[i]);
+            }
         }
     }
 }
