82c59b2045f60f2c804682dfed726cc65d53d9f0
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-07-07 20:46:58 +0900

    commit

---
+++ level3/SteppingStone.java
@@ -0,0 +1,19 @@
+package level3;
+import java.util.*;
+
+public class SteppingStone {
+    public static void main(String[] args){
+        int[] stones = {2,4,5,3,2,1,4,2,5,1};
+        int k = 3;
+    }
+
+    public static int solution(int[] stones, int k){
+        Arrays.sort(stones);
+
+        int min = stones[0];
+        int max = stones[stones.length-1];
+        int mid = (min+max)/2;
+
+        
+    }
+}
