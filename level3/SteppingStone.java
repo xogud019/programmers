d629202495d67456651a90224d1f4d7918be1c98
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-07-09 17:28:04 +0900

    commit

--- level3/SteppingStone.java
+++ level3/SteppingStone.java
@@ -5,6 +5,8 @@ public class SteppingStone {
     public static void main(String[] args){
         int[] stones = {2,4,5,3,2,1,4,2,5,1};
         int k = 3;
+        
+        System.out.println(solution(stones, k));
     }
 
     public static int solution(int[] stones, int k){
@@ -12,8 +14,33 @@ public class SteppingStone {
 
         int min = stones[0];
         int max = stones[stones.length-1];
-        int mid = (min+max)/2;
 
-        
+        while(min<max){
+            int mid = min+(max-min)/2;
+            int count=0;
+            boolean isT = false;
+
+            for(int i=0; i<stones.length; i++){
+                if(stones[i]-mid<0){
+                    count++;
+                }
+                else{
+                    count=0;
+                }
+
+                if(count==k){
+                    isT = true;
+                }
+            }
+            if(!isT){
+                min = mid+1;
+            }
+            else{
+                max =  mid;
+            }
+
+        }
+
+        return min;
     }
 }
