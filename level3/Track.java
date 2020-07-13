68b28ad429bf167bab9c748d6e7b442bc7ab516e
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-07-13 16:55:41 +0900

    commit

--- level3/Track.java
+++ level3/Track.java
@@ -3,57 +3,57 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Track {
-    static int[] pointX = {-1, 0, 1, 0};
-    static int[] pointY = {0, 1, 0, -1};
+    static int[] pointX = {1,0,-1,0};
+    static int[] pointY = {0,-1,0,1};
     public static void main(String[] args){
         int[][] board = {{0,0,0,0,0,0,0,1},{0,0,0,0,0,0,0,0},{0,0,0,0,0,1,0,0},{0,0,0,0,1,0,0,0},{0,0,0,1,0,0,0,1},{0,0,1,0,0,0,1,0},{0,1,0,0,0,1,0,0},{1,0,0,0,0,0,0,0}};
+
         System.out.println(solution(board));
     }
 
     public static int solution(int[][] board){
         int answer = Integer.MAX_VALUE;
-        int row = board.length;
-        int col = board[0].length;
+        int row = board.length, col = board[0].length;
         Queue<Car> q = new LinkedList<>();
 
         q.add(new Car(0,0,-1,0));
-        board[0][0]=1;
+        board[0][0] = 1;
 
         while(!q.isEmpty()){
             Car car = q.poll();
 
             if(car.x==row-1&&car.y==col-1){
-                answer = Math.min(answer, car.cost);
+                answer = Math.min(answer,car.cost);
                 continue;
             }
 
             for(int i=0; i<pointX.length; i++){
-                int newX = car.x+pointX[i];
-                int newY = car.y+pointY[i];
+                int newX = pointX[i]+car.x;
+                int newY = pointY[i]+car.y;
 
                 if(newX>=0 && newY>=0 && newX<row && newY<col && board[newX][newY]!=1){
-                    int newCost = 0;
+                    int temp = 0;
 
-                    if(car.dir==-1 || car.dir==i){
-                        newCost = car.cost+100;
+                    if(car.dir==-1 || car.dir == i){
+                        temp += car.cost + 100;
                     }
                     else if(car.dir!=i){
-                        newCost = car.cost+600;
+                        temp += car.cost + 600;
                     }
 
                     if(board[newX][newY]==0){
-                        board[newX][newY]=newCost;
-                        q.add(new Car(newX, newY, i, newCost));
+                        board[newX][newY] = temp;
+                        q.add(new Car(newX,newY, i, temp));
                     }
-                    else if(board[newX][newY]>=newCost){
-                        board[newX][newY]=newCost;
-                        q.add(new Car(newX, newY, i, newCost));
+                    else if(board[newX][newY]>=temp){
+                        board[newX][newY] = temp;
+                        q.add(new Car(newX,newY, i, temp));
                     }
                 }
             }
         }
 
-        //print(board);
+        print(board);
         return answer;
     }
 
