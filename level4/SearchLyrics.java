bbf4d24cefa87f9c15b54c4e189ff3f25032b521
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-07-15 20:53:38 +0900

    commit

---
+++ level4/SearchLyrics.java
@@ -0,0 +1,37 @@
+package level4;
+import java.util.*;
+
+public class SearchLyrics {
+    public static void main(String[] args){
+        String[] words = {"frodo", "front", "frost", "frozen", "frame", "kakao"};
+        String[] queries ={"fro??", "????o", "fr???", "fro???", "pro?"};
+
+        int[] answer = solution(words, queries);
+        for(int i:answer){
+            System.out.print(i+" ");
+        }
+    }
+    public static int[] solution(String[] words, String[] queries) {
+        int[] answer = new int[queries.length];
+        List<HashSet<String>> list = new ArrayList<>();
+        HashSet<String> set;
+        for(int i=0; i<queries.length; i++){
+            queries[i] = queries[i].replaceAll("\\?", ".");
+        }
+
+        for(String s: queries){
+            set = new HashSet<>();
+            for(String ss: words){
+                if(ss.matches(s)){
+                    set.add(ss);
+                }
+            }
+            list.add(set);
+        }
+
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i).size();
+        }
+        return answer;
+    }
+}
