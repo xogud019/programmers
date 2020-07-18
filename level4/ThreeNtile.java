cc2f1d20dded7f57f919859ce03cf9d0683d43d8
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-07-18 21:26:21 +0900

    commit

--- level4/ThreeNtile.java
+++ level4/ThreeNtile.java
@@ -2,35 +2,37 @@ package level4;
 
 public class ThreeNtile {
     static final int mod = 1000000007;
-    static int[] dp = new int[5001];
+    static long[] dp = new long[5001];
 
     public static void main(String[] args){
-        int n = 4;
+        int n = 10;
 
         System.out.println(solution(n));
     }
 
     public static int solution(int n){
-        int answer = 0;
+        long pre = 0;
+        dp[0] = 1;
+        dp[2] = 3;
 
-        return answer;
-    }
-
-    public static int dfs(int n){
         if(n%2==1){
             return 0;
         }
 
-        if(n==0){
-            return 2;
-        }
-        
         if(dp[n]!=0){
-            return dp[n];
+            return (int)(dp[n]%mod);
         }
-        else{
-            dp[n] = dp[n-2]*3;
-            dp[n] += dfs(n-2);
+
+        for(int i=4; i<=n; i+=2){
+            pre += dp[i-4]*2;
+            dp[i] =  dp[i-2]*3+pre;
         }
+
+        for(long i:dp){
+            if(i!=0){
+                System.out.println(i);
+            }
+        }
+        return (int)dp[n];
     }
 }
