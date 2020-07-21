29aa0f111519e7ebb28d13bdafdf6329ecff3bae
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-07-21 11:06:54 +0900

    commit

--- Hackers/Easy/CutTheStick.java
+++ Hackers/Easy/CutTheStick.java
@@ -1,16 +1,45 @@
 package Hackers.Easy;
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class CutTheStick {
     static int[] cutTheSticks(int[] arr) {
+        int[] answer = {};
+        List<Integer> list= new ArrayList<>();
+        List<Integer> len= new ArrayList<>();
+        
+        for(int i:arr){
+            list.add(i);
+        }
+        
+        len.add(list.size());
 
+        while(true){
+            int min = Collections.min(list);
+            
+            for(int i=0; i<list.size(); i++){
+                list.set(i,list.get(i)-min);
+                if(list.get(i)==0){
+                    list.remove(i);
+                    i--;
+                }
+            }
+            if(list.size()!=0){
+                len.add(list.size());
+            }
+            if(list.size()==1||list.size()==0){
+                break;
+            }
 
+        }
+
+        answer = new int[len.size()];
+
+        for(int i=0; i<answer.length; i++){
+            answer[i]=len.get(i);
+        }
+        
+        return answer;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
