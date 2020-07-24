d13357b797eeda7b08ed1df09ffa05890b7d7ca4
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-07-24 16:10:46 +0900

    commit

--- Hackers/Easy/ACMTeam.java
+++ Hackers/Easy/ACMTeam.java
@@ -1,11 +1,7 @@
 package Hackers.Easy;
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
 public class ACMTeam {
     static int[] acmTeam(String[] topic) {
         int[] answer= new int[2];
