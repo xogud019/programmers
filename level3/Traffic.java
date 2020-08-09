8b354926430ff3803434d1eb85afc908507fbac4
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-08-09 19:54:06 +0900

    commit

---
+++ level3/Traffic.java
@@ -0,0 +1,14 @@
+package level3;
+import java.util.*;
+
+public class Traffic{
+    public static void main(String[] args){
+
+    }
+
+    public static int solution(String[] lines){
+        int answer = 0;
+
+        return answer;
+    }
+}
