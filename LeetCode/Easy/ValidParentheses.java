ff24faeb028c2b0062d57f64f06ff337e1fa93ba
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-09-08 19:54:00 +0900

    commit

---
+++ LeetCode/Easy/ValidParentheses.java
@@ -0,0 +1,38 @@
+package LeetCode.Easy;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            char temp = s.charAt(i);
+            
+            if(temp=='('||temp=='{'||temp=='['){
+                stk.push(temp);
+                continue;
+            }
+            else{
+                if(stk.isEmpty()) return false;
+                if(temp==')'&&stk.peek()=='('){
+                    stk.pop();
+                    continue;
+                }
+                if(temp=='}'&&stk.peek()=='{'){
+                    stk.pop();
+                    continue;
+                }
+                if(temp==']'&&stk.peek()=='['){
+                    stk.pop();
+                    continue;
+                }
+                
+                return false;
+            }
+           
+        }
+        
+        return stk.size()==0?true:false;
+    }
+}
