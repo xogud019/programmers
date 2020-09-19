3e451f1a2784414e0e2aaa0163dbd3b38edaef44
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-09-19 22:22:23 +0900

    commit

---
+++ BJ/BruteForce/OTTPlus.java
@@ -0,0 +1,25 @@
+package BJ.BruteForce;
+import java.util.Scanner;
+
+public class OTTPlus {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int t = scan.nextInt();
+
+        for(int i=0; i<t; i++){
+            int n = scan.nextInt();
+            int answer = dfs(n);
+            System.out.println(answer);
+        }
+        
+        scan.close();
+    }
+
+    public static int dfs(int n){
+        if(n==0) return 1;
+        if(n<0) return 0;
+
+        return dfs(n-1)+dfs(n-2)+dfs(n-3);
+    }
+}
