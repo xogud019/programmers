1881505bb1350fdfbab0b6822179e21b1bff9fe2
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-09-28 19:39:22 +0900

    commit

--- Codility/MaxCounters.java
+++ Codility/MaxCounters.java
@@ -1,4 +1,28 @@
+package Codility;
+
 public class MaxCounters {
+    public int[] solution(int N, int[] A) {
+        // write your code in Java SE 8
+        int[] answer = new int[N];
+        int max = 0;
+        int temp = 0;
+        for(int i:A){
+            if(i<=N&&i>=1){
+                if(answer[i-1] < temp) answer[i-1] = temp+1;
+                else answer[i-1]++;
+                
+                if(max<answer[i-1]) max = answer[i-1];
+                
+            }
+            if(i==N+1) temp = max;
+            
+        }
+        
+        for(int i=0; i<answer.length; i++){
+            if(answer[i]<temp) answer[i] = temp;
+        }
+        return answer;
+    }
     /*failed => correct 25 efficient = 60;
     public int[] solution(int N, int[] A) {
         // write your code in Java SE 8
