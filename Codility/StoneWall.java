910360d80ef238cff16ae116551cae6611dd2487
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-10-02 20:28:24 +0900

    commit

---
+++ Codility/StoneWall.java
@@ -0,0 +1,26 @@
+package Codility;
+
+import java.util.*;
+
+public class StoneWall {
+    public int solution(int[] H) {
+        // write your code in Java SE 8
+        Stack<Integer> stk = new Stack<>();
+        int block = 0;
+        
+        for(int i:H){
+            while(!stk.isEmpty()&&stk.peek()>i){
+                stk.pop();
+            }
+            
+            if(!stk.isEmpty()&&stk.peek()==i) continue;
+            
+            if(stk.isEmpty()||stk.peek()<i){
+                block++;
+                stk.push(i);
+            }
+        }
+        
+        return block;
+    }
+}
