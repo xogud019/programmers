7be9b4ea47059eaf558ad34a73dbc249a43a8aae
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-10-05 20:10:56 +0900

    commit

---
+++ LeetCode/Tree/Easy/MaximumDepthofNaryTree.java
@@ -0,0 +1,17 @@
+package LeetCode.Tree.Easy;
+//try again
+public class MaximumDepthofNaryTree {
+    public int maxDepth(Node root) {
+        if(root == null) return 0;
+        
+        if(root.children == null || root.children.isEmpty()) return 1;
+        
+        int answer = -1;
+        
+        for(Node node : root.children){
+            answer = Math.max(answer, maxDepth(node));
+        }
+        
+        return answer+1;
+    }
+}
