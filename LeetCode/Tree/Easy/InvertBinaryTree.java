7b090da9497ad50177c41775b138f370c5628ea4
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-10-06 16:18:27 +0900

    commit

---
+++ LeetCode/Tree/Easy/InvertBinaryTree.java
@@ -0,0 +1,15 @@
+package LeetCode.Tree.Easy;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return root;
+        
+        TreeNode l = invertTree(root.left);
+        TreeNode r = invertTree(root.right);
+        
+        root.left = r;
+        root.right = l;
+        
+        return root;
+    }
+}
