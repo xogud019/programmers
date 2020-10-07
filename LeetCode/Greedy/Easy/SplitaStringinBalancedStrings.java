b26cc9080bd68d7c3479a2657ac37078142f002a
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-10-07 20:54:31 +0900

    commit

---
+++ LeetCode/Greedy/Easy/SplitaStringinBalancedStrings.java
@@ -0,0 +1,23 @@
+package LeetCode.Greedy.Easy;
+
+public class SplitaStringinBalancedStrings {
+    public int balancedStringSplit(String s) {
+        int answer = 0;
+        int l = 0, r = 0;
+        
+        for(int i=0; i<s.length(); ++i){
+            char temp = s.charAt(i);
+            
+            if(temp == 'L') l++;
+            else r++;
+            
+            if(l==r&&l!=0){
+                answer ++;
+                l = 0;
+                r = 0;
+            }
+        }
+        
+        return answer;
+    }
+}
