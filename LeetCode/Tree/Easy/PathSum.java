8d293e90fb152b5de3413e6128e7e623cf872a9a
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-10-13 18:57:09 +0900

    commit

---
+++ LeetCode/Tree/Easy/PathSum.java
@@ -0,0 +1,10 @@
+package LeetCode.Tree.Easy;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        if(sum == root.val && root.left == null && root.right == null) return true;
+        
+        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+    }
+}
---
+++ LeetCode/Tree/Easy/SecondMinimumNodeInABinaryTree.java
@@ -0,0 +1,27 @@
+package LeetCode.Tree.Easy;
+
+import java.util.*;
+
+public class SecondMinimumNodeInABinaryTree {
+    List<Integer> list;
+    
+    public int findSecondMinimumValue(TreeNode root) {
+        list = new ArrayList<>();
+        
+        inorder(root);
+        
+        if(list.size() <=1) return -1;
+        
+        Collections.sort(list);
+        
+        return list.get(1);
+    }
+    
+    public void inorder(TreeNode root){
+        if(root != null){
+            if(!list.contains(root.val)) list.add(root.val);
+            inorder(root.left);
+            inorder(root.right);
+        }
+    }
+}
