02cc8dfeff5e01ed2ad8b04731d76ebb8b1e6245
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-10-21 19:57:09 +0900

    commit

---
+++ LeetCode/Array/Easy/SquaresOfASortedArray.java
@@ -0,0 +1,15 @@
+package LeetCode.Array.Easy;
+
+import java.util.*;
+
+public class SquaresOfASortedArray {
+    public int[] sortedSquares(int[] A) {
+        for(int i=0; i<A.length; i++){
+            A[i] *= A[i];
+        }
+        
+        Arrays.sort(A);
+        
+        return A;
+    }
+}
