a09c5deb8f18168dab4e321ccf235927d27b175b
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-01 22:19:38 +0900

    commit

---
+++ Hackers/SQL/SelectAll.sql
@@ -0,0 +1,6 @@
+/*
+Query all columns (attributes) for every row in the CITY table.
+*/
+
+SELECT ID, NAME, COUNTRYCODE, DISTRICT, POPULATION
+FROM CITY
---
+++ LeetCode/Array/Easy/MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,15 @@
+package LeetCode.Array.Easy;
+
+public class MinimumCostToMoveChipsToTheSamePosition {
+    public int minCostToMoveChips(int[] position) {
+        int odd = 0;
+        int even = 0;
+        
+        for(int i:position){
+            if(i%2==0) even++;
+            else odd++;
+        }
+        
+        return Math.min(odd,even);
+    }
+}
