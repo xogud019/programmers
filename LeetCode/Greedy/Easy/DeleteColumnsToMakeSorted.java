9aea41bd9721576fe3832596a7d7de86c82ac660
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-04 21:26:32 +0900

    commit

---
+++ LeetCode/Greedy/Easy/DeleteColumnsToMakeSorted.java
@@ -0,0 +1,18 @@
+package LeetCode.Greedy.Easy;
+
+public class DeleteColumnsToMakeSorted {
+    public int minDeletionSize(String[] A) {
+        int answer = 0;
+        
+        for(int i=0; i<A[0].length(); i++){
+            for(int j=0; j<A.length-1; j++){
+                if(A[j].charAt(i)>A[j+1].charAt(i)){
+                    answer++;
+                    break;
+                }
+            }    
+        }
+        
+        return answer;
+    }
+}
