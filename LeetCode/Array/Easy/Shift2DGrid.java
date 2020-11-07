da37feacd335b25c4b0af30fee2aa0fecb5d4e6d
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-07 22:22:26 +0900

    commit

--- LeetCode/Array/Easy/Shift2DGrid.java
+++ LeetCode/Array/Easy/Shift2DGrid.java
@@ -4,6 +4,41 @@ import java.util.*;
 
 public class Shift2DGrid {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<List<Integer>> answer = new ArrayList<>();
+        int row = grid.length;
+        int col = grid[0].length;
+        
+        for(; k>0; k--){
+            int[][] arr = new int[row][col];
+            
+            for(int i=0; i<row; i++){
+                for(int j=0; j<col-1; j++){
+                    arr[i][j+1] = grid[i][j];
+                }
+            }
+            
+            for(int i=0; i<row-1; i++){
+                arr[i+1][0] = grid[i][col-1];
+            }
+            
+            arr[0][0] = grid[row-1][col-1];
+            //print(arr);
+            
+            grid = arr;
+        }
+        
+        for(int[] i:grid){
+            List<Integer> list = new ArrayList<>();
+            answer.add(list);
+            for(int j:i){
+                list.add(j);
+            }
+        }
+        
+        return answer;
+    }
+    /* failed case
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int row = grid.length;
         int col = grid[0].length;
         List<List<Integer>> answer = new ArrayList<>();
@@ -44,4 +79,5 @@ public class Shift2DGrid {
         
         return answer;
     }
+    */
 }
