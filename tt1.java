9846266c1230c051e33e34ccaa2f1f578277946b
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-08 22:31:58 +0900

    commit

--- tt1.java
+++ tt1.java
@@ -2,9 +2,8 @@ public class tt1{
     public int solution(int[] a, int[] b) {
         int answer = 0;
         
-        for(int i=0; i<a.length; i++){
-            answer += a[i]*b[i];
-        }
+        for(int i=0; i<a.length; i++) answer += a[i]*b[i];
+        
         
         return answer;
     }
