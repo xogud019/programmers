afe20d4e97cbb96f54a8be683406a646df1bf224
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-09 20:41:08 +0900

    commit

---
+++ LeetCode/Greedy/Easy/WaterBottles.java
@@ -0,0 +1,17 @@
+package LeetCode.Greedy.Easy;
+
+public class WaterBottles {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int answer = numBottles;
+        int empty = numBottles;
+        numBottles = 0;
+        
+        while(empty >= numExchange){
+            numBottles = empty/numExchange;
+            answer += numBottles;
+            empty = empty%numExchange + numBottles;
+        }
+        
+        return answer;
+    }
+}
