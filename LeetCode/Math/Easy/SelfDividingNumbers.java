91afb1bd9005728619e0d26ba8ddeff486299b7a
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-12 22:03:26 +0900

    commit

---
+++ LeetCode/Math/Easy/SelfDividingNumbers.java
@@ -0,0 +1,31 @@
+package LeetCode.Math.Easy;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class SelfDividingNumbers {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i=left; i<=right; i++){
+            if(self(i)) answer.add(i);
+        }
+        
+        return answer;
+    }
+    
+    public boolean self(int num){
+        int base = num;
+        
+        while(num>0){
+            int div = num%10;
+            
+            if(div == 0) return false;
+            if(base%div != 0) return false;
+            
+            num /=10;
+        }
+        
+        return true;
+    }
+}
