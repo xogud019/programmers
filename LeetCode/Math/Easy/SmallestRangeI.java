846692eb4f77cc7667831d3a4cdd6f1905a8b317
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-15 22:16:36 +0900

    commit

---
+++ LeetCode/Math/Easy/SmallestRangeI.java
@@ -0,0 +1,11 @@
+package LeetCode.Math.Easy;
+
+import java.util.Arrays;
+
+public class SmallestRangeI {
+    public int smallestRangeI(int[] A, int K) {
+        Arrays.sort(A);
+        
+        return Math.max(0, A[A.length-1]-A[0]-K*2);
+    }
+}
