2e50da518789fd50324965f8c0b6e1a3dd45e9d0
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-19 19:57:41 +0900

    commit

---
+++ LeetCode/Array/Easy/FairCandySwap.java
@@ -0,0 +1,62 @@
+package LeetCode.Array.Easy;
+
+public class FairCandySwap {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int a = 0, b = 0;
+        int[] answer = new int[2];
+        
+        for(int i:A)
+            a += i;
+        
+        for(int i:B)
+            b += i;
+        
+        i_loop : for(int i:A){
+            for(int j:B){
+                if(a-i+j == b-j+i){
+                    answer[0] = i;
+                    answer[1] = j;
+                    break i_loop;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    /*failed case time out
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int[] answer = new int[2];
+        
+        i_loop: for(int i=0; i<A.length; i++){
+           int aVal = A[i];
+           j_loop: for(int j=0; j<B.length; j++){
+                int bVal = B[j];
+               
+                A[i] = bVal;
+                B[j] = aVal;
+               
+                if(sum(A) == sum(B)){
+                   answer[0] = aVal;
+                   answer[1] = bVal;
+                   break i_loop;
+                }
+               
+                A[i] = aVal;
+                B[j] = bVal;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public int sum(int[] A){
+        int n = 0;
+        
+        for(int i : A) 
+            n += i;
+        
+        return n;
+    }
+    */
+    
+}
---
+++ LeetCode/Math/Easy/SurfaceAreaOf3DShapes.java
@@ -0,0 +1,58 @@
+package LeetCode.Math.Easy;
+
+public class SurfaceAreaOf3DShapes {
+    static int[] X = {-1, 0, 1, 0};
+    static int[] Y = {0, 1, 0, -1};
+    
+    public int surfaceArea(int[][] grid) {
+        int answer = 0;
+        int len = grid.length;
+        
+        for(int x=0; x<len; x++){
+            for(int y=0; y<len; y++){
+                if(grid[x][y] > 0) answer +=2;
+                
+                for(int k=0; k<4; k++){
+                    int newX = x + X[k];
+                    int newY = y + Y[k];
+                    int val = 0;
+                    
+                    if(newX >=0 && newX < len && newY >= 0 && newY < len) val = grid[newX][newY];
+                    
+                    answer += Math.max(grid[x][y] - val, 0);
+                }
+            }
+        }
+        
+        return answer;
+    }
+    /*failed case
+    public int surfaceArea(int[][] grid) {
+        int top = 0 ,bot = 0 , fro =0, back = 0, left = 0, right = 0, depth = 0;
+        int zero = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        
+        for(int[] i : grid){
+            for(int j : i){
+                if(j==0) zero++;
+            }
+        }
+        
+        top = row * col - zero;
+        bot = row * col - zero;
+        
+        for(int i=0; i<col; i++){
+            fro += grid[0][i];
+            back += grid[row-1][i];
+        }
+        
+        for(int i=0; i<row; i++){
+            left += grid[i][0];
+            right += grid[i][col-1];
+        }
+        
+        return top+bot+fro+back+left+right+depth;
+    }
+    */
+}
