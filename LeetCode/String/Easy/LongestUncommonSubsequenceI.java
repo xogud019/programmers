a5fc2b0d443868287960baa11d998cfc7824fb85
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-21 22:08:40 +0900

    commit

---
+++ LeetCode/String/Easy/LongestUncommonSubsequenceI.java
@@ -0,0 +1,8 @@
+package LeetCode.String.Easy;
+
+public class LongestUncommonSubsequenceI {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b)) return -1;
+        else return Math.max(a.length(), b.length());
+    }
+}
