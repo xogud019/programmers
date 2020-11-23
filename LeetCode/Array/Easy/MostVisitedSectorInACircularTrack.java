fde1b1d58b613da653cd8a96cdf6f6da308bf01f
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-11-23 21:00:07 +0900

    commit

---
+++ LeetCode/Array/Easy/MostVisitedSectorInACircularTrack.java
@@ -0,0 +1,23 @@
+package LeetCode.Array.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MostVisitedSectorInACircularTrack {
+    public List<Integer> mostVisited(int n, int[] rounds) {
+        List<Integer> answer = new ArrayList<>();
+        int start = rounds[0], end = rounds[rounds.length-1];
+        
+        if(end >= start){
+            for(int i=start; i<=end; i++) answer.add(i);
+            
+        }
+        else{
+            for(int i=1; i<=n; i++){
+                if(i >= start || i <= end) answer.add(i);
+            }
+        }
+        
+        return answer;
+    }
+}
