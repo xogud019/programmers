0e307513ff9c7599f5fb13ebdf9bc8bbcfb1e1b4
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-12-03 20:29:32 +0900

    commit

--- LeetCode/Array/Easy/DuplicateZeros.java
+++ LeetCode/Array/Easy/DuplicateZeros.java
@@ -1,5 +1,19 @@
 package LeetCode.Array.Easy;
 
 public class DuplicateZeros {
+    public void duplicateZeros(int[] arr) {
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i] == 0){
+                shift(arr,i+1);
+                arr[i+1] = 0;
+                i++;
+            }
+        }
+    }
     
+    public void shift(int[] arr, int idx){
+        for(int i=arr.length-1; i>idx; i--){
+            arr[i] = arr[i-1];
+        }
+    }
 }
