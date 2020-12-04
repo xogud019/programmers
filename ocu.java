24acdff52eeb4b527a65d80aa7d04a8ad9dce9cf
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-12-04 22:25:08 +0900

    commit

---
+++ ocu.java
@@ -0,0 +1,23 @@
+public class ocu {
+    public static void main(String[] args){
+        System.out.println(-128<<<2);
+
+    }
+
+    final class aa{
+
+    }
+    abstract class bb{
+
+    }
+    
+    protected class ss{
+
+    }
+    public static void swap(int a, int b){
+        int c;
+        c = a;
+        a = b;
+        b= c;
+    }
+}
