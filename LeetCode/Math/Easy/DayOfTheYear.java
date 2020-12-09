7c3cb793318ce7266409a90d2af64d6ebbf54910
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-12-09 21:43:36 +0900

    commit

---
+++ LeetCode/Math/Easy/DayOfTheYear.java
@@ -0,0 +1,19 @@
+package LeetCode.Math.Easy;
+
+public class DayOfTheYear{
+    public int dayOfYear(String date) {
+        String[] arr = date.split("-");
+        int answer = 0;
+        int[] month = {31,28,31,30,31,30,31,31,30,31,30,31};
+        for(int i=0;i<Integer.parseInt(arr[1])-1;i++){
+            answer += month[i];
+        }
+        if(leap(Integer.parseInt(arr[0])) && Integer.parseInt(arr[1]) > 2) answer++;
+        answer += Integer.parseInt(arr[2]);
+        return answer;
+    }
+    
+    public boolean leap(int i){
+        return (i%4==0 && i%100 !=0 )|| i %400==0;
+    }
+}
