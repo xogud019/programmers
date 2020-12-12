8d0102eba84eaf8a9eb316bebb427077a199acc3
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-12-12 20:34:34 +0900

    commit

---
+++ LeetCode/Math/Easy/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,13 @@
+package LeetCode.Math.Easy;
+
+import java.util.Arrays;
+
+public class MaximumProductOfThreeNumbers {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        
+        return Math.max(nums[len-1]*nums[len-2]*nums[len-3], nums[len-1]*nums[0]*nums[1]);
+        
+    }
+}
---
+++ LeetCode/String/Easy/NumberOfDaysBetweenTwoDates.java
@@ -0,0 +1,33 @@
+package LeetCode.String.Easy;
+
+public class NumberOfDaysBetweenTwoDates {
+    static int[] month = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    
+    public int daysBetweenDates(String date1, String date2) {
+        int day1 = 0, day2 = 0, mon1 = 0, mon2 = 0, yoon1 = 0, yoon2 = 0;
+        String[] d1 = date1.split("-");
+        String[] d2 = date2.split("-");
+        
+        //year+day
+        day1 += Integer.parseInt(d1[0])*365 + Integer.parseInt(d1[2]);
+        day2 += Integer.parseInt(d2[0])*365 + Integer.parseInt(d2[2]);
+        
+        //yoon
+        for(int i=1; i<Integer.parseInt(d1[0]); i++) if(yoon(i)) yoon1++;
+        for(int i=1; i<Integer.parseInt(d2[0]); i++) if(yoon(i)) yoon2++;
+        
+        if(Integer.parseInt(d1[1])>2 && yoon(Integer.parseInt(d1[0]))) yoon1++;
+        if(Integer.parseInt(d2[1])>2 && yoon(Integer.parseInt(d2[0]))) yoon2++;
+        
+        //month
+        for(int i=0; i<Integer.parseInt(d1[1]); i++) mon1 += month[i];
+        for(int i=0; i<Integer.parseInt(d2[1]); i++) mon2 += month[i];
+
+        return Math.abs((day1+mon1+yoon1)-(day2+mon2+yoon2));
+    }
+    
+    public boolean yoon(int n){
+        if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) return true;
+        else return false;
+    }
+}
