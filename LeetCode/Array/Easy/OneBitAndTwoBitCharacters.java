6250206e648e8bbef4f93988a4456b64ac6b4412
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-12-19 20:12:03 +0900

    commit

---
+++ LeetCode/Array/Easy/OneBitAndTwoBitCharacters.java
@@ -0,0 +1,19 @@
+package LeetCode.Array.Easy;
+
+public class OneBitAndTwoBitCharacters {
+    public boolean isOneBitCharacter(int[] bits) {
+        boolean answer = false;
+        
+        for(int i=0; i<bits.length; i++){
+            if((char)(bits[i]+'0') == '0'){
+                answer = true;
+            }   
+            else{
+                answer = false;
+                i++;
+            }
+        }
+        
+        return answer;
+    }
+}
