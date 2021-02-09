d961dcf7aa5d4bf69974cc657dc09c85f86cc0ee
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-02-09 19:16:30 +0900

    commit

--- LeetCode/Tree/Medium/BinarySearchTreeToGreaterSumTree.java
+++ LeetCode/Tree/Medium/BinarySearchTreeToGreaterSumTree.java
@@ -53,4 +53,18 @@ public class BinarySearchTreeToGreaterSumTree {
             dfs(root.left);
         }
     }
+    /*
+    int sum = 0;
+    
+    public TreeNode convertBST(TreeNode root) {
+        if(root == null) return null;
+        
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        
+        return root;
+    }
+    */
 }
