8030660425cc5a86452c6ee6941623454337a1ff
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-02-11 21:06:25 +0900

    commit

--- LeetCode/Sort/Easy/ValidAnagram.java
+++ LeetCode/Sort/Easy/ValidAnagram.java
@@ -3,6 +3,26 @@ package LeetCode.Sort.Easy;
 import java.util.Arrays;
 
 public class ValidAnagram {
+    /*hashMap
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(char c:s.toCharArray()) map.put(c, map.getOrDefault(c,0)+1);
+        
+        for(char c:t.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)-1);
+                
+                if(map.get(c)==0) map.remove(c);
+            }
+            else return false;
+        }
+        
+        return true;
+    }
+    */
     public boolean isAnagram(String s, String t) {
         char[] arr1 = s.toCharArray();
         char[] arr2 = t.toCharArray();
