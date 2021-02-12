309884bdd10c2f075f477ab0956e8aba34c3bf35
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-02-12 19:06:50 +0900

    commit

--- LeetCode/BitManifulation/Easy/NumberOfStepsToReduceANumberToZero.java
+++ LeetCode/BitManifulation/Easy/NumberOfStepsToReduceANumberToZero.java
@@ -13,4 +13,26 @@ public class NumberOfStepsToReduceANumberToZero {
         
         return answer;
     }
+
+    /*bitt man
+    public int numberOfSteps (int num) {
+        if(num == 0) return 0;
+        
+        return Integer.toBinaryString(num).length() + Integer.bitCount(num)-1;
+    }
+    Explanation:
+Example for numberOfSteps(5)
+Steps involved -
+1. 5 - 1 = 4
+2. 4/2 = 2
+3. 2/2 = 1
+4. 1 - 0 = 0
+Converting the above steps to binary gives us the following -
+1. 101 - 1 = 100
+2. 100 >> 1 = 10 (divided by 2)
+3. 10 >> 1 = 1
+4. 1 - 1 = 0
+Steps 1, 4 indicates the number of set bit count
+Steps 2, 3 indicates the total number of digits in the binary representation - 1. We are subtracting one as the end result is 0 which is being included in the binary representation's length
+    */
 }
