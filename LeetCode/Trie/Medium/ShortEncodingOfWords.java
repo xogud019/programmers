053960428c169f394a79f63616d666f34d7fb926
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-03-06 19:40:20 +0900

    commit

---
+++ LeetCode/Trie/Medium/ShortEncodingOfWords.java
@@ -0,0 +1,93 @@
+package LeetCode.Trie.Medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+A valid encoding of an array of words is any reference string s and array of indices indices such that:
+
+words.length == indices.length
+The reference string s ends with the '#' character.
+For each index indices[i], the substring of s starting from indices[i] and up to (but not including) the next '#' character is equal to words[i].
+Given an array of words, return the length of the shortest reference string s possible of any valid encoding of words.
+
+ 
+
+Example 1:
+
+Input: words = ["time", "me", "bell"]
+Output: 10
+Explanation: A valid encoding would be s = "time#bell#" and indices = [0, 2, 5].
+words[0] = "time", the substring of s starting from indices[0] = 0 to the next '#' is underlined in "time#bell#"
+words[1] = "me", the substring of s starting from indices[1] = 2 to the next '#' is underlined in "time#bell#"
+words[2] = "bell", the substring of s starting from indices[2] = 5 to the next '#' is underlined in "time#bell#"
+Example 2:
+
+Input: words = ["t"]
+Output: 2
+Explanation: A valid encoding would be s = "t#" and indices = [0].
+
+ 
+
+Constraints:
+
+1 <= words.length <= 2000
+1 <= words[i].length <= 7
+words[i] consists of only lowercase letters.
+*/
+public class ShortEncodingOfWords {
+    /*using set and prefix
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet<>();
+        int answer = 0;
+        
+        for(String word:words) set.add(word);
+        
+        for(String word:words){
+            for(int i=1; i<word.length(); i++){
+                set.remove(word.substring(i));
+            }
+        }
+        
+        for(String word:set) answer += word.length()+1;
+        
+        return answer;
+    }
+    */
+    public int minimumLengthEncoding(String[] words) {
+        TrieNode trie = new TrieNode();
+        Map<TrieNode, Integer> nodes = new HashMap<>();
+
+        for (int i = 0; i < words.length; ++i) {
+            String word = words[i];
+            TrieNode cur = trie;
+            for (int j = word.length() - 1; j >= 0; --j)
+                cur = cur.get(word.charAt(j));
+            nodes.put(cur, i);
+        }
+
+        int ans = 0;
+        for (TrieNode node: nodes.keySet()) {
+            if (node.count == 0)
+                ans += words[nodes.get(node)].length() + 1;
+        }
+        return ans;
+
+    }
+}
+
+class TrieNode {
+    TrieNode[] children;
+    int count;
+    TrieNode() {
+        children = new TrieNode[26];
+        count = 0;
+    }
+    public TrieNode get(char c) {
+        if (children[c-'a'] == null) {
+            children[c-'a'] = new TrieNode();
+            count++;
+        }
+        return children[c - 'a'];
+    }
+}
