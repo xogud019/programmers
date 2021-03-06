385b692775d04110d2dac3a14dcb27ab0e575b39
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-03-06 22:37:14 +0900

    commit

--- LeetCode/Trie/Medium/ShortEncodingOfWords.java
+++ LeetCode/Trie/Medium/ShortEncodingOfWords.java
@@ -91,3 +91,50 @@ class TrieNode {
         return children[c - 'a'];
     }
 }
+
+/*abstract
+public int minimumLengthEncoding(String[] words) {
+		Node trie = new Node();
+		// add each word to trie tree
+		for (String word : words) {
+			add(trie, word);
+		}
+		// collect lengths with recursion
+		return collect(trie);
+	}
+
+	private void add(Node trie, String word) {
+		Node node = trie;
+		for (int i = word.length() - 1; i >= 0; i--) {
+			node = node.get(word.charAt(i));
+		}
+		node.word = word;
+	}
+
+	private int collect(Node node) {
+		if (node == null)
+			return 0;
+		if (node.count == 0)
+			return node.word.length() + 1;
+		int count = 0;
+		for (Node n : node.next) {
+			count += collect(n);
+		}
+		return count;
+	}
+
+	class Node {
+		Node[] next = new Node[26];
+		int count = 0; // count children nodes, zero means a leave
+		String word = null;
+
+		public Node get(char c) {
+			int i = c - 'a';
+			if (next[i] == null) {
+				next[i] = new Node();
+				count++;
+			}
+			return next[i];
+		}
+	}
+*/
