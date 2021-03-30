bea7f1fc5029fc334775226f14c38c30ed0b42ea
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-03-30 20:10:35 +0900

    commit

--- exam.java
+++ exam.java
@@ -1,5 +1,5 @@
 import java.util.Queue;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,7 +14,7 @@ public class exam {
     }
     public static void main(String[] args){
         int[][] enter = {{1,3,2},{1,4,2,3},{3,2,1},{3,2,1},{1,4,2,3}}, leave = {{1,2,3},{2,1,3,4},{2,1,3},{1,3,2},{2,1,4,3}};
-
+       
         for(int i=0; i<enter.length; i++){
             int[] arr = solution(enter[i], leave[i]);
             print(arr);
@@ -24,7 +24,34 @@ public class exam {
     public static int[] solution(int[] enter, int[] leave){
         int len = enter.length;
         int[] answer  = new int[len];
+        Queue<Integer> out = new LinkedList<>();
+        List<Integer> in = new LinkedList<>();
         
+        Arrays.fill(answer, -1);
+
+        for(int i:leave) out.add(i);
+
+        for(int en:enter){
+            in.add(en);
+            int size = in.size();
+            answer[in.get(in.size()-1)-1] += size-1;
+
+            for(int i:in) answer[i-1]++;
+    
+            while(in.contains(out.peek())){
+                int idx = out.poll();
+
+                for(int i=0; i<in.size(); i++){
+                    if(idx == in.get(i)){
+                        idx = i;
+                        break;
+                    }
+                }
+
+                in.remove(idx);
+            }
+        }
+
         return answer;
     }
 
