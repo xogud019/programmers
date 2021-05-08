cedf6530551fe4ddb1b61985d4a91f27bd2003f0
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-05-08 13:45:48 +0900

    commit

--- LeetCode/DP/Easy/RangeSumQuery.java
+++ LeetCode/DP/Easy/RangeSumQuery.java
@@ -1,5 +1,36 @@
 package LeetCode.DP.Easy;
+/*
+Given an integer array nums, handle multiple queries of the following type:
 
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+
+Example 1:
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= left <= right < nums.length
+At most 104 calls will be made to sumRange.
+*/
 public class RangeSumQuery {
     class NumArray {
         int[] nums;
@@ -18,4 +49,23 @@ public class RangeSumQuery {
             return answer;
         }
     }
+
+    /*using dp
+    class NumArray {
+        int[] dp;
+        
+        public NumArray(int[] nums) {
+            dp = new int[nums.length];
+            dp[0] = nums[0];
+            
+            for(int i=1; i<nums.length; i++) dp[i] = nums[i] + dp[i-1];
+        }
+        
+        public int sumRange(int left, int right) {
+            if(left == 0) return dp[right];
+            else return dp[right] - dp[left-1];
+        }
+    }
+
+    */
 }
