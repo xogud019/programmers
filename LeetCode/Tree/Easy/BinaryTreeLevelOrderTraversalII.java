65233d5c56aa79785cc4c83645e88198deb07b54
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-05-20 17:11:55 +0900

    commit

--- LeetCode/Tree/Easy/BinaryTreeLevelOrderTraversalII.java
+++ LeetCode/Tree/Easy/BinaryTreeLevelOrderTraversalII.java
@@ -1,7 +1,31 @@
 package LeetCode.Tree.Easy;
 
 import java.util.*;
+/*
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
 
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+*/
 public class BinaryTreeLevelOrderTraversalII {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
@@ -41,4 +65,34 @@ public class BinaryTreeLevelOrderTraversalII {
         
         return answer;
     }
+
+    /*
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> list;
+        
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            list = new ArrayList<>();
+            
+            for(int i=0; i<size; i++){
+                TreeNode node = q.poll();
+                
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+                
+                list.add(node.val);
+            }
+            
+            answer.add(list);
+        }
+        
+        return answer;
+    }
+    */
 }
