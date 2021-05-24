36bac3067d8cb5e4103aa5d1c054edd25190d5b5
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-05-24 18:35:35 +0900

    commit

--- LeetCode/DP/Medium/CountingBits.java
+++ LeetCode/DP/Medium/CountingBits.java
@@ -150,4 +150,23 @@ public class CountingBits {
         return dp;
     }
     */
+
+    /*dp
+    public int[] countBits(int num) {
+        if(num == 0) return new int[]{0};
+        if(num == 1) return new int[]{0,1};
+        
+        int[] answer = new int[num+1];
+        
+        answer[0] = 0;
+        answer[1] = 1;
+        
+        for(int i=2; i<num+1; i++){
+            if(i%2 == 0) answer[i] = answer[i/2];
+            else answer[i] = answer[i-1]+1;
+        }
+        
+        return answer;
+    }
+    */
 }
