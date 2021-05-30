335917806ec9771f7d55ec0d81afae824646d381
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-05-30 17:59:16 +0900

    commit

--- LeetCode/DP/Medium/MinimumFallingPathSum.java
+++ LeetCode/DP/Medium/MinimumFallingPathSum.java
@@ -55,4 +55,22 @@ public class MinimumFallingPathSum {
         
         return answer;
     }
+    /*
+    public int minFallingPathSum(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        
+        for(int i=1; i<m; i++){
+            matrix[i][0] += Math.min(matrix[i-1][0], matrix[i-1][1]);
+            matrix[i][n-1] += Math.min(matrix[i-1][m-1],matrix[i-1][m-2]);
+            
+            for(int j=1; j<n-1; j++) matrix[i][j] += Math.min(matrix[i-1][j], Math.min(matrix[i-1][j-1],matrix[i-1][j+1]));
+        }
+        
+        int answer = Integer.MAX_VALUE;
+        
+        for(int i=0; i<n; i++) answer = Math.min(answer, matrix[m-1][i]);
+        
+        return answer;
+    }
+     */
 }
