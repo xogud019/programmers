dd01561fe31cbdc5a71dc11aeb47af300543516c
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-06-07 21:46:35 +0900

    commit

--- LeetCode/DP/Medium/UniquePaths.java
+++ LeetCode/DP/Medium/UniquePaths.java
@@ -51,4 +51,18 @@ public class UniquePaths {
         
         return board[m-1][n-1];
     }
+    /*
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(i == 1 || j == 1) dp[i][j] = 1;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m][n];
+    }
+    */
 }
--- LeetCode/DP/Medium/UniquePathsII.java
+++ LeetCode/DP/Medium/UniquePathsII.java
@@ -34,6 +34,7 @@ n == obstacleGrid[i].length
 obstacleGrid[i][j] is 0 or 1.
 */
 public class UniquePathsII {
+    //23ms
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         int[][] board = new int[m][n];
@@ -79,4 +80,29 @@ public class UniquePathsII {
         */
         return board[m-1][n-1];
     }
+    /*2ms
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        
+        if(obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1) return 0;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(obstacleGrid[i][j] == 1) obstacleGrid[i][j] = -1;
+            }
+        }
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(obstacleGrid[i][j] == -1) continue;
+                if(i == 0 && j == 0) obstacleGrid[i][j] = 1;
+                else if(i == 0) obstacleGrid[i][j] = obstacleGrid[i][j-1];
+                else if(j == 0) obstacleGrid[i][j] = obstacleGrid[i-1][j];
+                else obstacleGrid[i][j] = (obstacleGrid[i-1][j] == -1? 0 : obstacleGrid[i-1][j]) + (obstacleGrid[i][j-1] == -1? 0 : obstacleGrid[i][j-1]);
+            }
+        }
+        
+        return obstacleGrid[m-1][n-1];
+    }
+    */
 }
