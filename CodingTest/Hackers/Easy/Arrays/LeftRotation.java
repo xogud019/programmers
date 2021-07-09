8410db531536ce0715113a99d6e55e3f47a35263
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-07-09 19:38:29 +0900

    replace files

--- CodingTest/Hackers/Easy/Arrays/LeftRotation.java
+++ CodingTest/Hackers/Easy/Arrays/LeftRotation.java
@@ -1,16 +1,21 @@
-package Hackers.Easy.Sort;
+package CodingTest.Hackers.Easy.Arrays;
 import java.io.*;
 import java.util.*;
 
-public class CountingSortP1 {
-    static int[] countingSort(int[] arr) {
-        int[] answer = new int[100];
+public class LeftRotation {
+    static int[] rotLeft(int[] a, int d) {
+        int count = 0;
+        while(count<d){
+            int temp = a[0];
+            for(int i=1; i<a.length; i++){
+                a[i-1] = a[i];
+            }
 
-        for(int i:arr){
-            answer[i]++;
+            a[a.length-1] = temp;
+            count++;
         }
 
-        return answer;
+        return a;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -18,20 +23,23 @@ public class CountingSortP1 {
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        String[] nd = scanner.nextLine().split(" ");
 
-        int[] arr = new int[n];
+        int n = Integer.parseInt(nd[0]);
 
-        String[] arrItems = scanner.nextLine().split(" ");
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
         }
 
-        int[] result = countingSort(arr);
+        int[] result = rotLeft(a, d);
 
         for (int i = 0; i < result.length; i++) {
             bufferedWriter.write(String.valueOf(result[i]));
