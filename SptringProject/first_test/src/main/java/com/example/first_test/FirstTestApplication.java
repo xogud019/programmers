54f80e38bd475af1d2f154bda494c2cb7f266aa1
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-07-09 20:32:37 +0900

    spring test

---
+++ SptringProject/first_test/src/main/java/com/example/first_test/FirstTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTestApplication.class, args);
+	}
+
+}
