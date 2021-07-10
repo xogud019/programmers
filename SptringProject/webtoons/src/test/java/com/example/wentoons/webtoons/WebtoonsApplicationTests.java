e21878ef89aff33919f7c34243793d943101555e
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-07-10 22:18:10 +0900

    toy project.1

---
+++ SptringProject/webtoons/src/test/java/com/example/wentoons/webtoons/WebtoonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wentoons.webtoons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtoonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
