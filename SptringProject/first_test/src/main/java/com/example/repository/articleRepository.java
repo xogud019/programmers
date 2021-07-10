631757f869883d0080dee76a3620c735f5f8198f
xogud019 <57133962+xogud019@users.noreply.github.com>
2021-07-10 11:03:20 +0900

    sptring practice

--- SptringProject/first_test/src/main/java/com/example/repository/articleRepository.java
+++ SptringProject/first_test/src/main/java/com/example/repository/articleRepository.java
@@ -3,7 +3,9 @@ package com.example.repository;
 import com.example.Entity.Article;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface articleRepository extends CrudRepository<Article, Long>{
     
 }
